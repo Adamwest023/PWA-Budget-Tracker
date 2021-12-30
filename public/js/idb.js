d765058cbb8d3bf4bc312ae8577f4ca8d944a649
= <=>
2021-12-30 14:24:02 -0600

    added idb.js save function

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,10 +2,66 @@ let db;
 
 const request = indexedDB.open('budget-tracker', 1);
 
-request.onupgradeneeded = function(event) {
-    //save a reference to the database
+request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    // create an object store(table) called `new_item`, set it to have 
-    //an auto incrementing primary key of sorts
-    db.createObjectStore('new_item', {autoIncrement: true});
+    db.createObjectStore('new_item', { autoIncrement: true });
 };
+
+// upon a successful 
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadExpenses();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new item and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access the object store for `new_pizza`
+    const itemObjectStore = transaction.objectStore('new_item');
+
+    // add record to your store with add method
+    itemObjectStore.add(record);
+}
+
+//function to upload transaction
+function uploadExpenses() {
+    const transaction = db.transaction(['new_transaction', 'readwrite']);
+    const itemObjectStore = transaction.objectStore('new-transaction');
+    //get all records set to a variable
+    const getAll = itemObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((serverResponse) => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new-transaction'], 'readwrite');
+                    const itemObjectStore = transaction.objectStore('new-transaction');
+                    itemObjectStore.clear();
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
+    };
+}
+
+window.addEventListener('online', uploadExpenses);
