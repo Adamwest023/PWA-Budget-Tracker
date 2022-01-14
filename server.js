4c8fd45080f8e99c95bc97a371f425c21319cf96
= <=>
2022-01-14 08:55:56 -0600

    added index.js to a src folder

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-// app.use('/', express.static('public'));
+// app.get('/', express.static('public'));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
